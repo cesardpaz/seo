6c7c487a357da3fbed3ff4291ef0624caf782ab0
César De Paz <cesardpaz@gmail.com>
2023-04-28 14:48:23 -0500

    Added SEO

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+    title: 'Mi sitio web',
+    description: 'Descripción de mi sitio web',
+    openGraph: {
+      type: 'website',
+      locale: 'es_ES',
+      url: 'https://www.example.com/',
+      site_name: 'Mi sitio web',
+    },
+    twitter: {
+      handle: '@handle',
+      site: '@site',
+      cardType: 'summary_large_image',
+    },
+};
